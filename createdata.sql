7a9975a585cddc2c6b240a3ccdbe80258ee69766
ravdeepjohar <ravdeepsingjohar@live.com>
2013-10-18 23:57:45 -0400

    fix database

--- createdata.sql
+++ createdata.sql
@@ -1,12 +1,46 @@
-create table finstocks(
-	exchange varchar(10),
-	stock_symbol varchar(20),
-	stockdate date,
-	stock_price_open float ,
-	stock_price_high float,
-	stock_price_low float,
-	stock_price_close float,
-	stock_volume float,
-	stock_price_adj_close float,
-	PRIMARY KEY(stock_symbol,stockdate)
+create table customer(
+	name varchar(80),
+	city varchar(20),
+	ssn  integer PRIMARY KEY,
+	dob  date,
+	email varchar(40)
+)
+
+create table customerport(
+	ssn integer,
+	p_id integer,
+	CONSTRIANT fk_price FOREIGN KEY(ssn) REFERENCES customer(ssn),
+	CONSTRIANT fk_price FOREIGN KEY(p_id) REFERENCES portfolio(id)
+)
+
+create table stock(
+	name varchar(40) PRIMARY KEY,
+	symbol varchar(40),
+	m_id integer,
+	CONSTRIANT fk_price FOREIGN KEY(m_id) REFERENCES market(m_id)
+)
+
+create table price(
+	stock_date date,
+	open_pride float,
+	close_price float,
+	low_price float,
+	high_price float,
+	name varchar(80),
+	CONSTRIANT fk_price FOREIGN KEY(name) REFERENCES stock(name)
+)
+
+create table portfolio(
+	id integer PRIMARY KEY,
+	portfolio_name varchar(40),
+	name varchar(80),
+	CONSTRIANT fk_price FOREIGN KEY(name) REFERENCES stock(name)
+)
+
+create table market(
+	m_id integer PRIMARY KEY,
+	market_name varchar(40),
+	market_location varchar(40),
+	start_time time,
+	close_time time
 )
