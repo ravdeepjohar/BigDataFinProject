86fc9d4e2f4ee514b64da34d628bd5e443f4a4f7
ravdeepjohar <rsj7209@g.rit.edu>
2013-11-09 22:11:16 +0000

    create matplotlib view and delete some files

--- stock/urls.py
+++ stock/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^$', 'stock.stockspredict.views.home', name='home'),
+   (r'^charts/simple.png$', 'stock.stockspredict.views.simple'),
     url(r'^admin/', include(admin.site.urls)),
 )
