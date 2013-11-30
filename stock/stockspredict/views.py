e0f7e21dbc5f68f87829c22885188fa80baa557d
Ravdeep Johar <ravdeepsinghjohar@live.com>
2013-11-29 19:52:12 -0500

    add login

--- stock/stockspredict/views.py
+++ stock/stockspredict/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
 from datetime import datetime
 
-def home(request):
-    return render(request, 'index.html')
+# def home(request):
+#     return render(request, 'index.html')
 
 def profile(request):
     return render(request,'profile.html')
@@ -32,3 +34,23 @@ def simple(request):
     response=django.http.HttpResponse(content_type='image/png') 
     canvas.print_png(response)     
     return response
+
+
+def home(request):
+    state = "Please log in below..."
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+            else:
+                state = "Your account is not active, please contact the site admin."
+        else:
+            state = "Your username and/or password were incorrect."
+
+    return render_to_response('index.html',{'state':state, 'username': username})
