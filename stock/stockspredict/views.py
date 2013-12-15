41be9e70c77bd3d1982c15cb7a505e99f0ccb673
ravdeepjohar <rsj7209@g.rit.edu>
2013-12-15 19:35:46 +0000

    commit all

--- stock/stockspredict/views.py
+++ stock/stockspredict/views.py
@@ -48,16 +48,17 @@ def login_user(request):
     if request.POST:
         username = request.POST.get('username')
         password = request.POST.get('password')
-
+        stock(request)
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
                 #login(request, user)
-                stock(request)
+                #stock(request)
+                return render(request,"stock.html")
                 #state = "You're successfully logged in!"
             else:
                 state = "Your account is not active, please contact the site admin."
         else:
             state = "Your username and/or password were incorrect."
 
-    return render_to_response('auth.html',{'state':state, 'username': username},context_instance=RequestContext(request))
+    return render_to_response('index.html',{'state':state, 'username': username},context_instance=RequestContext(request))
